49cc14fc80fead6afa71775c23a1b0b23a2fde58
KingK21 <jionatil@gmail.com>
2020-11-03 16:45:47 -0500

    Uploaded updated code

---
+++ ProjectCodeBase/booksite-20201103T194130Z-001/booksite/env/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!c:\users\ksynv\documents\vsprojects\cen4010-project\projectcodebase\booksite-20201103t194130z-001\booksite\env\scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
--- ProjectCodeBase/readme.txt
+++
@@ -1,3 +0,0 @@
-Please only include code in this folder. No planning documents(UML diagrams, pseudocode etc.) should go here.
-Also, please do not put any specification documents in this folder, ie, no project assignment documents or stand up worksheets etc.
-Place these items in the appropriate "Planning" and "Documents" folders, respectively.
