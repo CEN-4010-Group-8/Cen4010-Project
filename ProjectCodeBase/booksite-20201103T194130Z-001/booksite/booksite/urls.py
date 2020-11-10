15adb5dc2464d25612f57aee6349e8eb882a14ca
KingK21 <jionatil@gmail.com>
2020-11-10 18:49:41 -0500

    Added Django Rest Framework Implemented Rest points for Book List

--- ProjectCodeBase/booksite-20201103T194130Z-001/booksite/booksite/urls.py
+++ ProjectCodeBase/booksite-20201103T194130Z-001/booksite/booksite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from booksiteapp.views import BookView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('booksiteapp/', include('booksiteapp.urls')),
+	path('', include('booksiteapp.urls')),
+    path('api/books', BookView.as_view()),
 ]
